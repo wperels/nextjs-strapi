b9a542ddbc5f2f8951508e0857c78fd7703c1801
WPerelstein <wperelstein@gmail.com>
2025-10-18 10:26:38 -0600

    corrected className

--- app/our-team/[slug]/page.jsx
+++ app/our-team/[slug]/page.jsx
@@ -55,7 +55,7 @@ export default async function Page({params}) {
   
    return (
     <div>
-      <div class="text-white relative bg-gray-700 px-14 py-16 -mx-8 -mt-7">
+      <div className="text-white relative bg-gray-700 px-14 py-16 -mx-8 -mt-7">
         <h2 className="text-6xl font-bold relative z-30">{member.name}</h2>
         <img className="object-cover absolute top-0 bottom-0 left-1/2 right-0 block w-1/2 h-full opacity-50 filter grayscale" src={`http://localhost:1337${member.photo.formats.medium.url}`}/>
           <div className="absolute z-20 w-80 bg-gradient-to-r from-gray-700 to-transparent h-full top-0 bottom-0 left-1/2"></div>
